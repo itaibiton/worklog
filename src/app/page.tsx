cec5ace99de5d21aa4cb14ad3e48a2dc8c711cc5
itaibiton <itaibiton@business.com>
2024-01-10 23:34:02 +0200

    progress with ideas and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,54 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { ComboboxDemo } from "@/components/ui/combobox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Menu } from "@/components/ui/Menu";
+
+import {
+	Menubar,
+	MenubarCheckboxItem,
+	MenubarContent,
+	MenubarItem,
+	MenubarMenu,
+	MenubarRadioGroup,
+	MenubarRadioItem,
+	MenubarSeparator,
+	MenubarShortcut,
+	MenubarSub,
+	MenubarSubContent,
+	MenubarSubTrigger,
+	MenubarTrigger,
+} from "@/components/ui/menubar";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Button>Welcome to worklog</Button>
+		<main className="flex min-h-screen flex-col p-8 border-t items-center">
+			<h1 className="text-xl text-card-foreground">Welcome to worklog</h1>
+			<p className="text-muted-foreground">you'll going to be productive.</p>
+			<Separator className="my-4" />
+			<div className="flex flex-col gap-4">
+				<div className="flex flex-col gap-2 min-w-[500px]  rounded">
+					<Label htmlFor="task">Enter your task & task label</Label>
+					<div className="flex gap-2">
+						<Input type="text" id="task" placeholder="Enter any task..." />
+						<ComboboxDemo />
+					</div>
+					<Button className="self-end">Add task</Button>
+				</div>
+			</div>
 		</main>
 	);
 }
+
+// Open form with cmd + j
---
+++ src/components/ui/combobox.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import * as React from "react";
+import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+} from "@/components/ui/command";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import { Input } from "./input";
+import { Plus } from "lucide-react";
+import { Separator } from "./separator";
+
+const frameworks = [
+	{
+		value: "next.js",
+		label: "Next.js",
+	},
+	{
+		value: "sveltekit",
+		label: "SvelteKit",
+	},
+	{
+		value: "nuxt.js",
+		label: "Nuxt.js",
+	},
+	{
+		value: "remix",
+		label: "Remix",
+	},
+	{
+		value: "astro",
+		label: "Astro",
+	},
+];
+
+export function ComboboxDemo() {
+	const [open, setOpen] = React.useState(false);
+	const [value, setValue] = React.useState("");
+
+	return (
+		<Popover open={open} onOpenChange={setOpen}>
+			<PopoverTrigger asChild>
+				<Button
+					variant="outline"
+					role="combobox"
+					aria-expanded={open}
+					className="w-[200px] justify-between"
+				>
+					{value
+						? frameworks.find((framework) => framework.value === value)?.label
+						: "Select framework..."}
+					<CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent className="w-[200px] p-0">
+				<Command>
+					<CommandInput placeholder="Search framework..." className="h-9" />
+					<CommandEmpty>No framework found.</CommandEmpty>
+					<CommandGroup>
+						{frameworks.map((framework) => (
+							<CommandItem
+								key={framework.value}
+								value={framework.value}
+								onSelect={(currentValue) => {
+									setValue(currentValue === value ? "" : currentValue);
+									setOpen(false);
+								}}
+							>
+								{framework.label}
+								<CheckIcon
+									className={cn(
+										"ml-auto h-4 w-4",
+										value === framework.value ? "opacity-100" : "opacity-0"
+									)}
+								/>
+							</CommandItem>
+						))}
+						<Separator className="my-2" />
+						<CommandItem key="Command add">
+							<div className="flex gap-1 items-center">
+								<Input placeholder="Add new label..." />
+								<Button variant="ghost" className="p-1">
+									<Plus width={16} height={16} />
+								</Button>
+							</div>
+						</CommandItem>
+					</CommandGroup>
+				</Command>
+			</PopoverContent>
+		</Popover>
+	);
+}
